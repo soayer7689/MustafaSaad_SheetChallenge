5b6a45b192893f5a5cf6a7bf2025349adcd5c21b
Shorok Atwa <72991188+shorokatwa14@users.noreply.github.com>
2023-02-03 02:51:57 +0200

    Update Vanya and Fence (Shorok Atwa).cpp

--- A/First Day/Vanya and Fence/Vanya and Fence (Shorok Atwa).cpp
+++ A/First Day/Vanya and Fence/Vanya and Fence (Shorok Atwa).cpp
@@ -10,13 +10,13 @@ int main()
     while (FriendsNum--)
     {
         cin >> height;
+        //  check if friends height smaller than or equal to fence
         if (height <= FenceHeight)
-            // if friends height to check if smaller than or equal to fence
             // width equal 1
             sum += 1;
-        //  if friends height to check if greater than to fence
-        // width equal 2
+        //  check if friends height greater than to fence
         else
+            // width equal 2
             sum += 2;
     }
     // print the minimum width
